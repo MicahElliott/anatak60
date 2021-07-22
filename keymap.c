b73895ead601890bf6c8cf7070335ba2da2f87fa
Micah Elliott <mde@micahelliott.com>
2021-07-22 10:20:37 -0700

    init

---
+++ keymap.c
@@ -0,0 +1,78 @@
+/* Copyright 2019 rominronin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// MacOS based definitions.
+#define K_SPCFN LT(SYMB, KC_SPACE) // Tap for space, hold for symbols layer
+#define K_PRVWD LALT(KC_LEFT)      // Previous word
+#define K_NXTWD LALT(KC_RIGHT)     // Next word
+#define K_LSTRT LGUI(KC_LEFT)      // Start of line
+#define K_LEND  LGUI(KC_RIGHT)     // End of line
+#define UNDO    LGUI(KC_Z)         // UNDO
+#define CUT     LGUI(KC_X)         // CUT
+#define COPY    LGUI(KC_C)         // COPY
+#define PASTE   LGUI(KC_V)         // PASTE
+
+
+#define GUI_LBRC    GUI_T(KC_LBRC)               // Tap for [, hold for Alt
+#define GUI_RBRC    GUI_T(KC_RBRC)               // Tap for ], hold for Alt
+
+
+enum layer_names {
+    BASE,
+    NUMB,
+    SYMB,
+    CURS,
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  QMKBEST = SAFE_RANGE,
+  QMKURL
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = LAYOUT_1_a(/* Base */
+    LT(NUMB,KC_EQL),   _______, KC_PIPE, GUI_RBRC,KC_RAPC, KC_SPC,  KC_MINS, KC_TILD, KC_PLUS, KC_COLN, KC_LAPO, GUI_LBRC,KC_BSLS, _______, LT(NUMB,KC_GRV),
+    SFT_T(KC_COLN),    KC_SLSH, KC_DOT,  KC_COMM, KC_H,    KC_K,    KC_INS,           KC_CAPS, KC_V,    KC_D,    KC_C,    KC_X,    KC_Z,    SFT_T(KC_SCLN),
+    CTL_T(KC_ENT),     KC_O,    KC_I,    KC_E,    KC_N,    KC_M,    KC_RCBR,          KC_LCBR, KC_G,    KC_T,    KC_S,    KC_R,    KC_A,    CTL_T(KC_TAB),
+    LT(SYMB,KC_BSPC),  KC_QUOT, KC_Y,    KC_U,    KC_L,    KC_J,    KC_RABK, _______, KC_LABK, KC_B,    KC_P,    KC_F,    KC_W,    KC_Q,    LT(SYMB,KC_ESC),
+    _______,           KC_EQL,  KC_UNDS, KC_ASTR, KC_DLR,                   _______,          KC_CIRC, KC_PERC, KC_HASH, KC_GRV,  KC_SPC,  _______
+  ),
+  [NUMB] = LAYOUT_1_a(
+    _______,           _______, _______, _______, _______, _______, _______, DF(0),   _______, _______, _______, _______, _______, _______, _______,
+    _______,           KC_SLSH, KC_3,    KC_2,    KC_1,    KC_GT,   _______,          _______, KC_LT,   KC_PIPE, _______, KC_RPRN, KC_LPRN, _______,
+    _______,           KC_ASTR, KC_6,    KC_5,    KC_4,    KC_RCBR, _______,          _______, KC_LCBR, KC_PIPE, _______, KC_BSLS, KC_AMPR, _______,
+    _______,           KC_0,    KC_9,    KC_8,    KC_7,    KC_CIRC, _______, _______, _______, KC_PERC, KC_DLR,  KC_HASH, KC_AT,   KC_EXLM, KC_TILD,
+
+    _______,           _______, _______, _______,          _______, KC_P0,      _______,       _______, KC_PDOT, KC_PENT, _______, _______
+  ),
+  [SYMB] = LAYOUT_1_a(
+    KC_DOWN,           KC_END,  KC_F12,  KC_F11,  KC_F10,  _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, _______,
+    KC_UP,             KC_PGDN, KC_F3,   KC_F2,   KC_F1,   _______, _______,          _______, _______, _______, _______, KC_MUTE, KC_VOLU, _______,
+    _______,           KC_PGUP, KC_F6,   KC_F5,   KC_F4,   KC_DEL,  _______,          _______, _______, KC_RGHT, KC_DOWN, KC_LEFT, KC_PGDN, _______,
+    _______,           KC_HOME, KC_F9,   KC_F8,   KC_F7,   KC_INS,  _______, _______, _______, _______, KC_END,  KC_UP,   KC_HOME, KC_PGUP, _______,
+
+    _______,           _______, _______, _______,                _______,    _______,    KC_DEL,        _______, _______, _______, _______, _______
+  ),
+  [CURS] = LAYOUT_1_a(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,     _______, _______, _______, _______, _______, _______, _______, _______, K_PRVWD, KC_UP,   K_NXTWD, _______,      _______,
+    _______,   _______, KC_LCTL, KC_LALT, KC_LSFT, _______, _______,      _______, KC_BSPC, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,    _______,
+    _______, UNDO,    CUT,     COPY,    PASTE,   _______, _______, _______, _______, _______, K_LSTRT, _______, K_LEND,  _______, _______,
+    _______, _______, _______, _______,                _______,    _______,    _______,       _______, _______, _______, _______, _______
+  )
+};
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Anatak60 Keyboard Layout
+
+This is an upside-down variant of Katana60.
+
+## Usage
+
+```
+git clone https://github.com/qmk/qmk_firmware ~/src
+cd ~/src/qmk_firmware/keyboards/rominronin/katana60/rev2/keymaps
+git clone https://github.com/micahelliott/anatak60
+cd ~/src/qmk_firmware
+qmk flash -kb rominronin/katana60/rev2 -km anatak60
+```
+
+## About
+
+I needed a symmetric staggered keyboard that would aid with
+same-finger utilization (SFU).
+
+## Acquiring, Setup
+
+1. Order a [Katana60 from Candykeys](https://candykeys.com/product/katana60-pcb-V2).
+1. Solder switches of your choosing, put on some caps.
+1. Set up [QMK firmware](https://github.com/qmk/qmk_firmware).
+
+## Caveats
+
+- This layout will undergo some tweaks.
+- The keymap.c file is upside-down and backwards, could do some  work
+  to fix that.
