bf796b71b87ebc7c919ef0f8c3d7da384a26dc11
Micah Elliott <mde@micahelliott.com>
2025-04-10 13:24:28 -0700

    bdpx stnrf gvclk

--- keymap.c
+++ keymap.c
@@ -85,12 +85,12 @@ const uint16_t PROGMEM combo_gt[] = {KC_G,    KC_T,    COMBO_END};
 /* const uint16_t PROGMEM combo_lw[] = {KC_L,    KC_W,    COMBO_END}; */
 const uint16_t PROGMEM combo_bc[] = {KC_B, KC_C,   COMBO_END};
 /* const uint16_t PROGMEM combo_fd[] = {KC_F, KC_D,   COMBO_END}; */
-const uint16_t PROGMEM combo_df[] = {KC_D, KC_F,   COMBO_END};
-const uint16_t PROGMEM combo_bp[] = {KC_B, KC_P,   COMBO_END};
+/* const uint16_t PROGMEM combo_df[] = {KC_D, KC_F,   COMBO_END}; */
+const uint16_t PROGMEM combo_bd[] = {KC_B, KC_D,   COMBO_END};
 const uint16_t PROGMEM combo_scb[] = {KC_SCLN, KC_B,   COMBO_END};
 /* const uint16_t PROGMEM combo_cf[] = {KC_F,    KC_C,    COMBO_END}; */
 /* const uint16_t PROGMEM combo_vf[] = {KC_V,    KC_F,    COMBO_END}; */
-const uint16_t PROGMEM combo_pn[] = {KC_P,    KC_N,    COMBO_END};
+/* const uint16_t PROGMEM combo_pn[] = {KC_P,    KC_N,    COMBO_END}; */
 const uint16_t PROGMEM combo_bt[] = {KC_B,    KC_T,    COMBO_END};
 /* const uint16_t PROGMEM combo_dt[] = {KC_D,    KC_T,    COMBO_END}; */
 /* const uint16_t PROGMEM combo_kv[] = {KC_K,    KC_V,    COMBO_END}; */
@@ -98,7 +98,7 @@ const uint16_t PROGMEM combo_bt[] = {KC_B,    KC_T,    COMBO_END};
 /* const uint16_t PROGMEM combo_mh[] = {KC_M,    KC_H,    COMBO_END}; */
 /* const uint16_t PROGMEM combo_kl[] = {KC_K,    KC_L,    COMBO_END}; */
 const uint16_t PROGMEM combo_gv[] = {KC_G,    KC_V,    COMBO_END};
-/* const uint16_t PROGMEM combo_pf[] = {KC_P,    KC_F,    COMBO_END}; */
+const uint16_t PROGMEM combo_pf[] = {KC_P,    KC_F,    COMBO_END};
 /* const uint16_t PROGMEM combo_vf[] = {KC_V,    KC_F,    COMBO_END}; // unused */
 const uint16_t PROGMEM test_combo11[] = {KC_COMM, KC_MINS, COMBO_END};
 const uint16_t PROGMEM test_combo22[] = {KC_HASH, KC_Y,  COMBO_END};
@@ -115,12 +115,12 @@ combo_t key_combos[] = {
     /* COMBO(combo_qb, KC_A), */
     /* COMBO(combo_td, KC_J), */
     /* COMBO(combo_fd, KC_V), */
-    COMBO(combo_df, KC_DLR), // TODO
+    /* COMBO(combo_df, KC_DLR), // TODO */
     COMBO(combo_pd, KC_DLR), // TODO
-    COMBO(combo_bp, KC_Q),
+    COMBO(combo_bd, KC_Q),
     COMBO(combo_scb, KC_A),
     /* COMBO(combo_fp, KC_E), */ // FIXME
-    COMBO(combo_pn, KC_DLR), // TODO
+    /* COMBO(combo_pn, KC_DLR), // TODO */
     COMBO(combo_bt, LCTL(KC_TAB)),
     /* COMBO(combo_dt, KC_J), // TODO good one! */
     /* COMBO(combo_gt, LCTL(KC_G)), */
@@ -133,7 +133,7 @@ combo_t key_combos[] = {
     /* COMBO(combo_lw, KC_DLR), */
     /* COMBO(combo_vf, LCTL(KC_X)), */
     /* COMBO(combo_kl, KC_DLR), // TODO */
-    COMBO(combo_xf, KC_PIPE),
+    COMBO(combo_xf, KC_PIPE), // TODO
     COMBO(combo_bc, KC_DLR), // TODO
     /* COMBO(combo_cf, KC_COLN), */
     /* COMBO(combo_dp, KC_APP), */ // TODO
@@ -143,7 +143,7 @@ combo_t key_combos[] = {
     COMBO(test_combo24, KC_CIRC),
     COMBO(test_combo25, KC_DLR),
     COMBO(combo_gv, KC_Z),
-    /* COMBO(combo_pf, KC_PIPE), */
+    COMBO(combo_pf, KC_PIPE),
     /* COMBO(combo_gv, KC_APP), */
     /* COMBO(combo_gv, LCTL(KC_A)), */
     // COMBO(test_combo2, LCTL(KC_W)),
@@ -195,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(NUMB,KC_GRV),   _______, K_SCRN,  GUI_RBRC,SC_RAPC, KC_SPC,    KC_BSPC, KC_CAPS,      OSM(MOD_LSFT),QK_REP,  SC_LAPO, GUI_LBRC,SUPTAB, _______, LT(NUMB,KC_BSLS),
     KC_MINS,           KC_COMM, KC_DOT,  KC_SLSH, KC_Y,  KC_HASH,   KC_DLR,                  KC_EXLM,      KC_K,    KC_L,    KC_C,    KC_V,   KC_G,    KC_W,
     CTL_T(KC_ENT),     KC_A,    KC_I,    KC_E,    HN,      LCTL(KC_C),KC_RABK,               KC_LABK,      KC_F,    KC_R,    KC_N,    KC_T,   KC_S,    CTL_T(KC_TAB),
-    KC_DQUO,           KC_QUOT, KC_U,    KC_O,    KC_H,    KC_F5,     KC_AT,   KC_CAPS,      KC_TILD,      KC_X,    KC_D,    KC_P,    KC_B,   KC_SCLN, LT(SYMB,KC_GRV),
+    KC_DQUO,           KC_QUOT, KC_U,    KC_O,    KC_H,    KC_F5,     KC_AT,   KC_CAPS,      KC_TILD,      KC_X,    KC_P,    KC_D,    KC_B,   KC_SCLN, LT(SYMB,KC_GRV),
     KC_ESC,            KC_RCBR, KC_PLUS, KC_UNDS, KC_ASTR,                     CW_TOGG,                    KC_ESC,  KC_PERC, KC_EQL,  KC_Q,   KC_LCBR, KC_BSPC
                       ),
   [NUMB] = LAYOUT_1_a(
